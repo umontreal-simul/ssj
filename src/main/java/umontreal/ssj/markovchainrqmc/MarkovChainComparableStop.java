faee8634b992b51f47821a4e7a6ee1919c2a5a1d
Wyean Chan <chanwyea@iro.umontreal.ca>
2016-10-12 12:52:27 -0400

    Remove Deprecated classes and interfaces in markovchainrqmc

--- src/main/java/umontreal/ssj/markovchainrqmc/MarkovChainComparableStop.java
+++
@@ -1,9 +0,0 @@
-package umontreal.ssj.markovchainrqmc;
-
-/**
- * DEPRECATED: use `MarkovChainComparable`
- *
- * <div class="SSJ-bigskip"></div><div class="SSJ-bigskip"></div>
- */
-@Deprecated
-public abstract class MarkovChainComparableStop extends MarkovChainComparable  { }
--- src/main/java/umontreal/ssj/markovchainrqmc/MarkovChainDoubleStop.java
+++
@@ -1,9 +0,0 @@
-package umontreal.ssj.markovchainrqmc;
-
-/**
- * DEPRECATED: use `MarkovChainDouble`
- *
- * <div class="SSJ-bigskip"></div><div class="SSJ-bigskip"></div>
- */
-@Deprecated
-public abstract class MarkovChainDoubleStop extends MarkovChainDouble  { }
