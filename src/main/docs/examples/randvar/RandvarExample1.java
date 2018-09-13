f1c169bae33cf26c8ab4fe9fba35509b691665d3
atoka\Lecuyer <lecuyer@iro.umontreal.ca>
2018-09-13 14:22:36 -0400

    some updates in randvar

--- src/main/docs/examples/randvar/RandvarExample1.java
+++ src/main/docs/examples/randvar/RandvarExample1.java
@@ -3,8 +3,9 @@ package randvar;
 import umontreal.ssj.probdist.*;
 import umontreal.ssj.rng.*;
 import umontreal.ssj.randvar.*;
+import java.util.Arrays;
 
-public class normaltest {
+public class RandvarExample1 {
 
 	// Generate and print n random variates with generator gen.
 	private static void generate(RandomVariateGen gen, int n) {
@@ -23,9 +24,18 @@ public class normaltest {
 		RandomVariateGen gen1 = new NormalGen (new MRG31k3p());
 		RandomVariateGen gen2 = new NormalGen (new MRG31k3p(), 5.0, 121.4);
 		RandomVariateGen gen3 = new GammaGen (new MRG31k3p(), 2.0, 10.0);
-		generate(gen0, 3);  // Generate 3 standard normal variates from gen0
-		generate(gen1, 5);  // then 5 more from gen1
+		RandomVariateGenInt gen4 = new PoissonGen (new MRG31k3p(), 10.0);
+		
+		System.out.println ("Some normal, gamma, and Poisson variates \n");
+		generate(gen0, 3);  // Generate and prints 3 standard normal variates from gen0
+		generate(gen1, 5);  // then 5 more standard normals from gen1
 		generate(gen2, 3);  // then 3 normal variates with mean 5 and standard deviation 121.4 
 		generate(gen3, 2);  // then 2 gamma variates with parameters (2, 10).
+		int[] arrayP = gen4.nextArrayOfInt (10000); // Generate an array of 10000 Poisson variates of mean 10.
+		Arrays.sort(arrayP);  
+		System.out.println ("50% quantile from Poisson(10) variates: " + arrayP[5000]);		
+		System.out.println ("90% quantile from Poisson(10) variates: " + arrayP[9000]);		
+		System.out.println ("99% quantile from Poisson(10) variates: " + arrayP[9900]);		
+		System.out.println ("99.9% quantile from Poisson(10) variates: " + arrayP[9990]);		
 	}
 }
