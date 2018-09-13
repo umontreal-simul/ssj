66417ba035b5b83c40e2f91a23b5b690a2f250fc
Florian Puchhammer <florian.puchhammer@gmx.at>
2018-09-13 17:12:22 -0400

    Added DEModelBandwidthBased. Incomplete!

--- out/html/search/variables_c.js
+++ out/html/search/variables_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['rac2',['RAC2',['../classumontreal_1_1ssj_1_1util_1_1Num.html#a0b22089b39adb19fefdf014105ad08bd',1,'umontreal::ssj::util::Num']]]
+  ['plain',['PLAIN',['../classumontreal_1_1ssj_1_1util_1_1TableFormat.html#a7b46db0d046247251297d5eab96bd6d0',1,'umontreal::ssj::util::TableFormat']]],
+  ['produceplots',['producePlots',['../classumontreal_1_1ssj_1_1stat_1_1density_1_1DEModelBandwidthBased.html#adc150f1e73b5dcc77641c25769fa8fbd',1,'umontreal::ssj::stat::density::DEModelBandwidthBased']]]
 ];
--- out/html/search/variables_f.js
+++ out/html/search/variables_f.js
@@ -1,8 +1,12 @@
 var searchData=
 [
-  ['week',['WEEK',['../enumumontreal_1_1ssj_1_1util_1_1TimeUnit.html#a75fcbc1d8ec2ee41ee86c0418b184e11',1,'umontreal::ssj::util::TimeUnit']]],
-  ['weights',['weights',['../classumontreal_1_1ssj_1_1randvar_1_1MixtureGen.html#a7d927e125ac9a62e5aff2893ce50ebdd',1,'umontreal::ssj::randvar::MixtureGen']]],
-  ['weightsdist',['weightsDist',['../classumontreal_1_1ssj_1_1randvar_1_1MixtureGen.html#a37fb1ad043b671c658a130f27904d3ca',1,'umontreal::ssj::randvar::MixtureGen']]],
-  ['wg',['WG',['../classumontreal_1_1ssj_1_1gof_1_1GofFormat.html#ad94b2e6e5e285e89a76d11f0ad1b2bd4',1,'umontreal::ssj::gof::GofFormat']]],
-  ['wu',['WU',['../classumontreal_1_1ssj_1_1gof_1_1GofFormat.html#adeaa265ac52dd70b847177a844137d0f',1,'umontreal::ssj::gof::GofFormat']]]
+  ['ten_5fneg_5fpow',['TEN_NEG_POW',['../classumontreal_1_1ssj_1_1util_1_1Num.html#aff91ea7c045f008cfbe13245538e413e',1,'umontreal::ssj::util::Num']]],
+  ['testnames',['TESTNAMES',['../classumontreal_1_1ssj_1_1gof_1_1GofFormat.html#a978b93882259f55413ea26dc6179ab32',1,'umontreal::ssj::gof::GofFormat']]],
+  ['twoexp',['TWOEXP',['../classumontreal_1_1ssj_1_1util_1_1Num.html#a8f755eefb16e54ce09bab33cb1dfc6cc',1,'umontreal::ssj::util::Num']]],
+  ['type',['type',['../classumontreal_1_1ssj_1_1charts_1_1CustomHistogramDataset.html#ad2887cdedf223c36abd77c8829112fd4',1,'umontreal::ssj::charts::CustomHistogramDataset']]],
+  ['typechar_5fdouble',['TYPECHAR_DOUBLE',['../classumontreal_1_1ssj_1_1util_1_1io_1_1BinaryDataWriter.html#aa29554ab8ecd054feeca7889064a53be',1,'umontreal::ssj::util::io::BinaryDataWriter']]],
+  ['typechar_5ffloat',['TYPECHAR_FLOAT',['../classumontreal_1_1ssj_1_1util_1_1io_1_1BinaryDataWriter.html#ab3c32cfab8031cf31d3b88f32c1df2e8',1,'umontreal::ssj::util::io::BinaryDataWriter']]],
+  ['typechar_5finteger',['TYPECHAR_INTEGER',['../classumontreal_1_1ssj_1_1util_1_1io_1_1BinaryDataWriter.html#aaff66c29defac9b569cbe7c6c560a73e',1,'umontreal::ssj::util::io::BinaryDataWriter']]],
+  ['typechar_5flabel',['TYPECHAR_LABEL',['../classumontreal_1_1ssj_1_1util_1_1io_1_1BinaryDataWriter.html#a30f8c348b588bc9f64ea1ac02bffe152',1,'umontreal::ssj::util::io::BinaryDataWriter']]],
+  ['typechar_5fstring',['TYPECHAR_STRING',['../classumontreal_1_1ssj_1_1util_1_1io_1_1BinaryDataWriter.html#acad13763da777d9f384d8724ff571c56',1,'umontreal::ssj::util::io::BinaryDataWriter']]]
 ];
