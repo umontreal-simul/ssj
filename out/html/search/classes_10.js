a93b8a5dd3424809ca469cd95bcc9743623b14b7
Florian Puchhammer <florian.puchhammer@gmx.at>
2018-09-17 16:06:02 -0400

    trial

--- out/html/search/classes_10.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['qmcsampler',['QMCSampler',['../classumontreal_1_1ssj_1_1mcqmctools_1_1anova_1_1QMCSampler.html',1,'umontreal::ssj::mcqmctools::anova']]],
-  ['qqplot',['QQPlot',['../classumontreal_1_1ssj_1_1charts_1_1QQPlot.html',1,'umontreal::ssj::charts']]],
-  ['queueev',['QueueEv',['../classtutorial_1_1QueueEv.html',1,'tutorial']]],
-  ['queuelindley',['QueueLindley',['../classtutorial_1_1QueueLindley.html',1,'tutorial']]],
-  ['queueobs',['QueueObs',['../classtutorial_1_1QueueObs.html',1,'tutorial']]]
-];
